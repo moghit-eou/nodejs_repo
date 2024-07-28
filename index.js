a3b2b3a44762c2d16992c48e02e70953d0a96273
moghit-eou <moghit.elouaad1@gmail.com>
2024-07-28 20:59:51 +0100

    just learnt how to post a request

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-const express = require('express') //ini web server on our computer
-const app = express() // call it as function and call it app
+const express = require('express');
+const app = express()// call it as function and call it app
 const port = 3000 // equivalent to domain
-
-// defining the routes
-// ( '/' ) is the home route which is localhost:8080
-// moghit is a route localhost:8080/moghit
-// we can response
-const home_route = '/'
-function response_function(request , response)
-{
-    return response.send("fffffff one thing")
-}
 
 
 app.use(express.static('front_end_files'))
-// look for in the dir front_end_files and send its files
+app.use(express.json())
 
-app.get( home_route , response_function)
+let route = '/'
+app.get( route , (request, response ) => { 
+    response.status(200).send('<h1>JUST SAKE</h1>')
+} ) 
 
-    // response.send("message")  this will display a msg as well 
-    // we can send antying response.sendstatus(200 ) for example 
-    // response.send({json formate})
- 
+const friends = {
+}
+
+app.get('/friends' , (request, response) =>{
+   // response.status(200).send('<h1>get friend request</h1>')
+    response.status(200).send(friends) // sending the "friends" object
+})
+app.post('/friends' , (request , response ) => {
+    const { best_friend } = request.body 
+    friends[best_friend] = "is mdr"
+    response.sendStatus(200)
+}) // if we want to create a new friend
+
+app.patch('/' , (request , response ) => {
+    
+})
+
+app.put('/' , (request , response ) => {
+    
+})
+
+app.delete('/' , (request , response ) => {
+    
+})
+
 
 function display_server()
 {
     console.log("the sever is running and you can print anything here ")
 }
-
 app.listen(port,  display_server )
     // telling our server to listen to requests from this port 
